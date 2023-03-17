6a60d5ace29228f47de74d203af201f8c18c92dc
BetterthanNothin <sino.mostafa@gmail.com>
2023-03-17 18:39:35 -0400

    Create calendars are funny.py

---
+++ calendars are funny.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+def time_to_number(date: str) -> int:
+    i = date.split(":")
+    return int(i[0])*60 + int(i[1])
+
+
+def number_to_time(num: int) -> str:
+    return str(num//60) + ":" + str(num % 60)
+
+
+def load_file(file):
+    useless, start_times, end_times = np.loadtxt(file, dtype=str, delimiter=",", unpack=True)
+    return [start_times[1:], end_times[1:]]
+
+def do(file_name) -> str:
+    x = load_file(file_name)
+    pairs = []
+    total = []
+    for i in range(len(x[0])):
+        pairs.append([time_to_number(x[0][i]), time_to_number(x[1][i])])
+        total.extend([time_to_number(x[0][i]), time_to_number(x[1][i])])
+    total.sort()
+    rang = 0
+    start = 0
+    for t in range(len(total) - 1):
+        st = total[t]
+        et = total[t + 1]
+        good = True
+        for i in pairs:
+            if i[0] == st or i[1] == et:
+                good = False
+
+        if good and (et - st) > rang:
+            rang = et - st
+            start = st
+
+    if rang == 0:
+        return "None"
+    return "Start Time: " + number_to_time(start) + ", Range: " + str(rang)
+
+
+if __name__ == "__main__":
+    print(do("test1.csv"))
+
+
+
+
